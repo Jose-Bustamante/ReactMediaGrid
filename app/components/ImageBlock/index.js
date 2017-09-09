6476dcafe4e259f9bb354f5bb1c2c2427c333bb7
Bustamante, Jose <jose.bustamante@mobgen.com>
2017-09-09 17:11:29 +0200

    Initial commit

---
+++ app/components/ImageBlock/index.js
@@ -0,0 +1,25 @@
+/**
+*
+* ImageBlock
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pxToRem } from '../../utils/styleUtils';
+import Wrapper from './Wrapper';
+
+
+function ImageBlock({ source }) {
+  return (
+    <Wrapper>
+      <img src={source} alt="tweet" style={{ height: '100%', width: '100%' }} />
+    </Wrapper>
+  );
+}
+
+ImageBlock.propTypes = {
+  source: PropTypes.string.isRequired,
+};
+
+export default ImageBlock;
